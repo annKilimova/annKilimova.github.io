07ab9c84e9ecdf2e6f497eb36f5e1b897e2870e7
Anny <34607397+annKilimova@users.noreply.github.com>
2018-01-04 12:09:22 +0200

    new
    2018

--- js/Link.js
+++ js/Link.js
@@ -1,9 +1,8 @@
 /*Плавный переход к нужному блоку с якорными ссылками - begin*/
-jQuery('a[href^="#"]').click(function ($) {
-    var el = jQuery(this).attr('href');
-    event.preventDefault();
-    var id = jQuery(this).attr('href'),
-        top = jQuery(id).offset().top;
-    jQuery('body,html').animate({scrollTop: top}, 1500);
-});    
+jQuery(document).on('click', '.scrollto', function(event){
+       event.preventDefault();
+       jQuery('html, body').animate({
+           scrollTop: $( $.attr(this, 'href') ).offset().top
+       }, 500);
+   });
 /*(просто скопировать, ничего не менять)...end*/
